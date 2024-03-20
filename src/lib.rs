cc6660b331bb9b621cb045a28ced93b5e0507255
H2Sxxa <h2sxxa0w0@gmail.com>
2024-03-20 08:06:43 +0800

    🐛 remove CI test and move test code to `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub mod fields;
-mod test;
 pub mod webvpn;
+
+#[cfg(test)]
+mod test {
+    use super::webvpn::WebVpnClient;
+
+    #[tokio::test]
+    async fn login_test() {
+        let client = WebVpnClient::new("学号", "密码");
+        match client.sso_login().await {
+            Ok(json) => println!("{}", json),
+            Err(message) => println!("{}", message),
+        };
+    }
+}
